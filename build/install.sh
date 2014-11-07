05149750da7eb7427fc00594cfa1adbb2636ae0b
Allan Chappell <generalredneck@gmail.com>
2014-11-07 12:26:16 -0600

    Making Drupal Framework Install not fail when a mail system doesn't exist
    By setting send mail to true for the one command, we are circumventing drupal
    install from failing because there is no way to set the mail system before
    install however, drupal does send an email to the site administrator in order
    to let them know that a new version is ready of drupal.

--- build/install.sh
+++ build/install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 path=$(dirname "$0")
+true=`which true`
 source $path/common.sh
 
 echo "Installing Drupal minimal profile.";
-$drush si minimal --account-name=admin --account-pass=drupaladm1n
+# Setting PHP Options so that we don't fail while sending mail if a mail sytem
+# doesn't exist.
+PHP_OPTIONS="-d sendmail_path=$true" $drush si minimal --account-name=admin --account-pass=drupaladm1n
 source $path/update.sh
