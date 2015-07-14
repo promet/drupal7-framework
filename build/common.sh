6062ca9380c71532df8a7f44f95ab07b476dfd1a
Doug Dobrzynski <doug@prometsource.com>
2015-07-14 13:47:18 -0500

    Using Drush from Composer during the build.

--- build/common.sh
+++ build/common.sh
@@ -3,7 +3,7 @@
 set -e
 path=$(dirname "$0")
 base=$(cd $path/.. && pwd)
-drush="$base/vendor/bin/drush $drush_flags -y -r $base/www"
+drush="$PWD/vendor/bin/drush $drush_flags -y -r $base/www"
 
 if [[ -f .env ]]; then
   source .env
