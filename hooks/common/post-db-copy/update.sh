448c8576094937c399e9c3daec1e5174c0336d16
Allan Chappell <generalredneck@gmail.com>
2015-12-10 16:28:01 -0600

    Adding Acquia Hooks so that the framework is built with acquia compatibility.

---
+++ hooks/common/post-db-copy/update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+path=`dirname "$0"`
+site="$1"
+target_env="$2"
+source_branch="$3"
+deployed_tag="$4"
+repo_url="$5"
+repo_type="$6"
+
+echo "### Run Update Script ###"
+echo "$path/../../../build/update.sh -d \"drush @$site.$target_env\""
+$path/../../../build/update.sh -d "drush @$site.$target_env"
---
+++ hooks/prod/post-code-deploy/update.sh
@@ -0,0 +1,1 @@
+../../common/post-db-copy/update.sh
